2ee5e8595b11ba4d5914b2176662dcf2b02a8eae
bastelbude-01 <messerschmidt.markus@web.de>
2023-10-16 21:13:17 +0200

    changed cam.launch

---
+++ leo/launch/camera.launch.py
@@ -0,0 +1,26 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    color = Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            output='screen',
+            parameters=[{
+                'image_size': [640,480],
+                'camera_frame_id': 'camera_link_optical'
+                }]
+        )
+
+    BW= Node(
+            package='pi_rover',
+            executable='image_publisher',
+        )
+
+    return LaunchDescription([
+
+    BW        
+        
+    ])
