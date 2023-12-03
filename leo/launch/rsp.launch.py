0ba564de3073482fccc6d85107c3fa8f50326cb5
bastelbude-01 <mod3llfr3ak1mg3ist@web.de>
2023-12-03 18:20:28 +0100

    ...

--- leo/launch/rsp.launch.py
+++ leo/launch/rsp.launch.py
@@ -57,6 +57,6 @@ def generate_launch_description():
         #    default_value='true',
         #    description='Use ros2_control if true'),
 
-        node_robot_state_publisher,
-        rviz2
+        node_robot_state_publisher#,
+        #rviz2
     ])
