dcfbaf342f8835b603b13a0623e5d40a88dfc5d6
bastelbude-01 <mod3llfr3ak1mg3ist@web.de>
2023-12-07 23:42:22 +0100

    working in the turret

--- leo/launch/joystick.launch.py
+++ leo/launch/joystick.launch.py
@@ -6,26 +6,36 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    joy_params = os.path.join(get_package_share_directory("leo"), "config", "joystick.yaml")
+    joy_params1 = os.path.join(get_package_share_directory("leo"), "config", "joystick.yaml")
+    joy_params2 = os.path.join(get_package_share_directory("leo"), "config", "joystickTurret.yaml")
 
     joystick = Node(
             package='joy',
             executable='joy_node',
-            parameters=[joy_params]
+            parameters=[joy_params1]
         )
     
-    teleop_node = Node(
+    teleop_node1 = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
-            name='teleop_node',
-            parameters=[joy_params],
+            name='teleop_node1',
+            parameters=[joy_params1],
             remappings=[("/cmd_vel", "/leo_driver/cmd_vel_unstamped")]
+        )
+
+    teleop_node2 = Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='teleop_node2',
+            parameters=[joy_params2],
+            remappings=[("/cmd_vel", "/leo_turret")]
         )
 
 
     return LaunchDescription([
 
         joystick,
-        teleop_node
+        teleop_node1,
+        teleop_node2
         
     ])
