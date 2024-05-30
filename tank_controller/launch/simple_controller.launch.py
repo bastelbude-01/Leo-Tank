5648267f5e3365d17959aed7b24d06bff5c71770
bastelbude-01 <mod3llfr3ak1mg3ist@web.de>
2024-05-30 12:28:35 +0200

    in work

--- tank_controller/launch/simple_controller.launch.py
+++ tank_controller/launch/simple_controller.launch.py
@@ -22,9 +22,31 @@ def generate_launch_description():
                     "/controller_manager"
                 ]
             )
+    
+    turret_controller =  Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "turret_controller",
+                    "--controller-manager",
+                    "/controller_manager"
+                ]
+            )
+    
+    pipe_controller =  Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=[
+                    "pipe_controller",
+                    "--controller-manager",
+                    "/controller_manager"
+                ]
+            )
 
 
     return LaunchDescription([
         joint_state_broadcaster_spawner,
-        simple_controller
+        simple_controller,
+        turret_controller#,
+        #pipe_controller
     ])
