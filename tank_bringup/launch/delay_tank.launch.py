277549068c88d3f8759149984a27d3d7dcef5460
bastelbude-01 <mod3llfr3ak1mg3ist@web.de>
2024-07-02 14:38:29 +0200

    delayed launcher for the serial ports

---
+++ tank_bringup/launch/delay_tank.launch.py
@@ -0,0 +1,55 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+import os
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    
+
+    hardware_interface_chassis = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("chassis_firmware"),  
+            "launch",                                           
+            "hardware_interface.launch.py"                     
+        )
+    )
+
+    hardware_interface_turret = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("turret_firmware"),  
+            "launch",                                           
+            "hardware_interface.launch.py"                     
+        )
+    )
+
+    hardware_interface_pipe = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("pipe_firmware"),  
+            "launch",                                           
+            "hardware_interface.launch.py"                     
+        )
+    )
+
+    controller = IncludeLaunchDescription(
+        os.path.join(
+            get_package_share_directory("tank_controller"),  
+            "launch",                                             
+            "controller.launch.py"                                
+        ),
+        launch_arguments={
+            "use_simple_controller": "false",
+            "use_python": "False"
+        }.items()
+    )
+
+
+    return LaunchDescription([
+        controller,
+        TimerAction(period=5.0, actions=[hardware_interface_chassis]),
+        TimerAction(period=5.0, actions=[hardware_interface_turret])#,
+        #TimerAction(period=5.0, actions=[hardware_interface_pipe])
+        
+
+
+    ])
